b8551fd27a87b5e9c4e5f58dd10c40e7cfabe62a
RehanDossa <rehandossa44@gmail.com>
2024-09-26 18:40:16 +0530

    intial commit

--- paractice/src/App.js
+++ paractice/src/App.js
@@ -3,21 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div>
+      <h1>Profile</h1>
+      <div className="Profile">
+        <p className="name">Rehan</p>
+        <img className="Profile"src="https://www.shutterstock.com/image-illustration/small-yellow-compact-car-260nw-394558741.jpg" alt=""></img>
+        <p className="about">I AM A DEVELOPER</p>
+      </div>
     </div>
   );
 }
