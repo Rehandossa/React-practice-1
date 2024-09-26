53b434440cddf04321acab10cf572ecce1d2e977
RehanDossa <rehandossa44@gmail.com>
2024-09-26 19:38:10 +0530

    intial commit

--- paractice/src/App.js
+++ paractice/src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './components/profile';
 
 function App() {
   return (
-   
     <div>
-      <h1>Profile</h1>
-      <div className="Profile">
-        <p className="name">Rehan</p>
-        <img className="Profile"src="https://www.shutterstock.com/image-illustration/small-yellow-compact-car-260nw-394558741.jpg" alt=""></img>
-        <p className="about">I AM A DEVELOPER</p>
-      </div>
+      <h1>profile</h1>
+      <Profile />
     </div>
   );
 }
---
+++ paractice/src/components/profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Profile() {
+    return (
+        <div>
+            <div className="profile">
+                <p className="name">Rehan</p>
+                <img className="Profile"src="https://cdn.pixabay.com/photo/2024/02/29/13/03/ai-generated-8604396_640.png" alt=""/>
+                <p className="about">I AM A DEVELOPER</p>
+            </div>
+        </div>
+   );
+}
+
+export default Profile;
