30841a52c3cff5d5396175f353363b9acb26519f
RehanDossa <rehandossa44@gmail.com>
2024-09-30 19:38:11 +0530

    intial commit

--- paractice/src/components/profile.js
+++ paractice/src/components/profile.js
@@ -1,15 +1,30 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from "react-bootstrap/Card";
 
-function Profile({name,pic,About}) {
-    return (
-        <div>
-            <div className="profile">
-                <p className="name">MY NAME IS {name}</p>
-                <img className="Profile"src={pic} alt=""/>
-                <p className="about">{About}</p>
-            </div>
-        </div>
-   );    
+function Profile({ name, pic, About,profileUrl }) {
+  return (
+    <div>
+      <Card className="mb-5" style={{ width: "18rem" }}>
+        <Card.Img
+          variant="top"
+          src={pic}
+        />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>{About}</Card.Text>
+          <Card.Link href={profileUrl} target="_blank"><Button variant="primary">View {name}'s Profile </Button></Card.Link>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default Profile;
+{
+  /* <div className="profile">
+                <p className="name">MY NAME IS {name}</p>
+                <img className="Profile"src={pic} alt=""/>
+                <p className="about">{About}</p>
+            </div> */
+}
