d20e347a152dd1ac2f9286c9efc978341039a678
RehanDossa <rehandossa44@gmail.com>
2024-10-01 19:37:21 +0530

    intial commit

--- paractice/src/components/profile.js
+++ paractice/src/components/profile.js
@@ -2,21 +2,19 @@ import React from "react";
 import Button from 'react-bootstrap/Button';
 import Card from "react-bootstrap/Card";
 
-function Profile({ name, pic, About,profileUrl }) {
-  return (
-    <div>
-      <Card className="mb-5" style={{ width: "18rem" }}>
+function Profile({profile}) {
+  return (  
+      <Card className="col-md-4 m-3" style={{ width: "18rem", display: "flex" }}>
         <Card.Img
           variant="top"
-          src={pic}
+          src={profile.img}
         />
         <Card.Body>
-          <Card.Title>{name}</Card.Title>
-          <Card.Text>{About}</Card.Text>
-          <Card.Link href={profileUrl} target="_blank"><Button variant="primary">View {name}'s Profile </Button></Card.Link>
+          <Card.Title>{profile.name}</Card.Title>
+          <Card.Text>{profile.About}</Card.Text>
+          <Card.Link href={profile.profileUrl} target="_blank"><Button variant="primary">View {profile.name}'s Profile </Button></Card.Link>
         </Card.Body>
       </Card>
-    </div>
   );
 }
 
