a04d52179535d511f2d7427853344f67f2b0788f
RehanDossa <rehandossa44@gmail.com>
2024-10-03 19:33:27 +0530

    intial commit

--- paractice/src/App.js
+++ paractice/src/App.js
@@ -5,12 +5,13 @@ import Card from "react-bootstrap/Card";
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [data , setData] = useState({
+  const [data, setData] = useState({
     name: "",
-    age: ""
-  })
+    Loginpassword: "",
+    Email: "",
+  });
 
-  const [isEmpty, setIsEmpty] = useState(true); 
+  const [isEmpty, setIsEmpty] = useState(true);
 
   // const [profiles, setProfiles] = useState([]);
   // useEffect(() => {
@@ -20,41 +21,64 @@ function App() {
   //     .catch((error) => console.error("Error fetching the profiles:", error));
   // }, []);
 
+  const UpdateName = (e) => {
+    setData({ ...data, name: e.target.value });
+    setIsEmpty(
+      e.target.value == "" && data.name == "" && e.target.value == ""
+        ? true
+        : false
+    );
+  };
+  const UpdateEmail = (e) => {
+    setData({ ...data, Email: e.target.value });
+    setIsEmpty(
+      e.target.value == "" && e.target.value == "" && data.Email == ""
+        ? true
+        : false
+    );
+  };
+  const UpdateLoginPassword = (e) => {
+    setData({ ...data, Loginpassword: e.target.value });
+    setIsEmpty(
+      data.Loginpassword == "" && e.target.value == "" && e.target.value == ""
+        ? true
+        : false
+    );
+  };
+
   return (
-    <div className="container">
-      {
-        isEmpty ?
-          <h1>No Data</h1>
-        :
+    <div className="container ">
+      <h1>Hello</h1>
+      {isEmpty ? (
+        <h1>No Data</h1>
+      ) : (
         <>
-        {
-          data.name != '' ? <h1>Name: {data.name}</h1> : <></>
-        }
-        {
-          data.age != '' ? <h1>Age: {data.age}</h1> : <></>
-          
-        }
+          {data.name != "" ? <h1>Name: {data.name}</h1> : <></>}
+          {data.Email != "" ? <h1>Email: {data.Email}</h1> : <></>}
+          {data.Loginpassword != "" ? (
+            <h1>Login password: {data.Loginpassword}</h1>
+          ) : (
+            <></>
+          )}
         </>
-      }
-      <input type="text" onChange={
-        (e)=>{
-          setData({
-            ...data,
-            name: e.target.value
-          })
-          setIsEmpty(e.target.value == '' && data.age == '' ? true : false)
-        }
-      }/>
-      <input type="text" onChange={
-        (e)=>{
-          setData({
-            ...data,
-            age: e.target.value
-          })
-          setIsEmpty(data.name == '' &&  e.target.value == '' ? true : false)
-          }}/>
+      )}
+      <input
+        type="text"
+        placeholder="Name"
+        onChange={UpdateName}
+      />
+      <input
+        type="text"
+        placeholder="Email"
+        onChange={UpdateEmail}
+      />
+      <input
+        type="password"
+        placeholder="Login password"
+        onChange={UpdateLoginPassword}
+      />
     </div>
-  )
+  );
 
   // return (
   //   <div className="container mt-5 mb-5">
